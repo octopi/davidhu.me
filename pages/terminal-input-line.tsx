db39833d849ac5cd6ebae9bf9c0ced98459efcc4
David Hu <octopie@gmail.com>
2022-12-31 16:39:08 -0500

    autocomplete basics

--- pages/terminal-input-line.tsx
+++ pages/terminal-input-line.tsx
@@ -3,17 +3,22 @@ import React, { useRef } from "react";
 type TerminalInputLineProps = {
   command: string;
   updateCommand: (command: string) => void;
+  canAutocomplete: boolean;
+  autocomplete: string;
 };
 
 export default function TerminalInputLine({
   command,
   updateCommand,
+  canAutocomplete,
+  autocomplete,
 }: TerminalInputLineProps) {
   const defaultCommand = useRef(command);
-
   const handleChange = (e: React.ChangeEvent<HTMLSpanElement>) => {
     updateCommand(e.currentTarget.textContent || "");
   };
+  const shouldShowAutocomplete =
+    canAutocomplete && autocomplete.substring(0, command.length) === command;
 
   return (
     <div className="absolute m-2 before:text-gray-500 before:content-['$']">
@@ -23,9 +28,11 @@ export default function TerminalInputLine({
         onInput={handleChange}
         dangerouslySetInnerHTML={{ __html: defaultCommand.current }}
       />
-      <span className="font-mono text-sm italic text-gray-500">
-        url thething.com
-      </span>
+      {shouldShowAutocomplete && (
+        <span className="font-mono text-sm italic text-gray-500">
+          {autocomplete.substring(command.length)}
+        </span>
+      )}
     </div>
   );
 }
--- pages/terminal.tsx
+++ pages/terminal.tsx
@@ -5,9 +5,9 @@ export default function Terminal() {
   const [command, setCommand] = useState<string>("c");
   return (
     <div className="w-2/3 min-w-[400px] max-w-2xl rounded-lg shadow-2xl shadow-gray-900">
-      <div className="relative h-10 rounded-t-lg bg-[rgba(27,27,34,1)] before:absolute before:left-1 before:m-3 before:h-4 before:w-4 before:rounded-full before:bg-[rgba(60,60,60,1)] before:shadow-[1.4em_0em_rgba(60,60,60,1),2.8em_0em_rgba(60,60,60,1)] before:content-['']"></div>
+      <div className="relative h-9 rounded-t-lg bg-[rgba(27,27,34,1)] before:absolute before:left-1 before:m-3 before:h-3 before:w-3 before:rounded-full before:bg-[rgba(60,60,60,1)] before:shadow-[1.2em_0em_rgba(60,60,60,1),2.4em_0em_rgba(60,60,60,1)] before:content-['']"></div>
       <div className="relative h-96 rounded-b-lg bg-[rgba(18,18,18,1)] font-mono">
-        <TerminalInputLine command={command} updateCommand={setCommand} />
+        <TerminalInputLine command={command} updateCommand={setCommand} canAutocomplete={true} autocomplete="curl thething.com" />
       </div>
     </div>
   );
